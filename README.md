d912589b59793ccb6b908505318b5547c8c0cf19
Seonghyeon Cho <seonghyeoncho96@gmail.com>
2023-08-12 18:20:47 +0900

    wip

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 ## go-zookeeper-practice
 
 Test zookeeper watcher event with go-zookeeper, testcontainers
+
+### Usage
+
+```sh
+docker run --rm -p 2181:2181 --name zk zookeeper
+```
+Run zookeeper locally, before run
+
+
--- main.go
+++ main.go
@@ -1,73 +1,83 @@
 package main
 
 import (
-	"context"
-	"fmt"
 	"log"
 	"time"
 
 	"github.com/go-zookeeper/zk"
-	"github.com/testcontainers/testcontainers-go"
-	"github.com/testcontainers/testcontainers-go/wait"
 )
 
 func main() {
-	ctx := context.Background()
-	zkC, err := setupZk(ctx)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer func(zkC testcontainers.Container, ctx context.Context) {
-		err := zkC.Terminate(ctx)
-		if err != nil {
-			log.Fatalf("Failed to terminate container: %v", err.Error())
-		}
-	}(zkC, ctx)
-
-	port, _ := zkC.MappedPort(ctx, "2181/tcp")
-	zkTest(port.Int())
+	zkTest()
 }
 
-func setupZk(ctx context.Context) (testcontainers.Container, error) {
-	req := testcontainers.ContainerRequest{
-		Image:        "zookeeper:3.6.2",
-		ExposedPorts: []string{"2181/tcp"},
-		WaitingFor:   wait.ForListeningPort("2181/tcp"),
-	}
-	zkC, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
-		ContainerRequest: req,
-		Started:          true,
-	})
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return zkC, err
-}
-
-func zkTest(mappedPort int) {
-	zkAddr := []string{fmt.Sprintf("127.0.0.1:%d", mappedPort)}
-	conn, ec, err := zk.Connect(zkAddr, time.Second*10)
+func zkTest() {
+	zkAddr := []string{"127.0.0.1:2181"}
+	conn, _, err := zk.Connect(zkAddr, time.Second*10)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer conn.Close()
 
-	for e := range ec {
-		switch e.Type {
-		case zk.EventNodeCreated:
-			log.Println("zk.EventNodeCreated")
-		case zk.EventNodeDeleted:
-			log.Println("zk.EventNodeDeleted")
-		case zk.EventNodeDataChanged:
-			log.Println("zk.EventNodeDataChanged")
-		case zk.EventNodeChildrenChanged:
-			log.Println("zk.EventNodeChildrenChanged")
-		case zk.EventSession:
-			log.Println("zk.EventSession")
-		case zk.EventNotWatching:
-			log.Println("zk.EventNotWatching")
+	for {
+		println("Try to watch /serverset")
+		// XXX: What happens if the node does not exist, or node is deleted?
+		_, _, events, err := conn.ChildrenW("/serverset")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		/*
+			type Event struct {
+				Type   EventType
+				State  State
+				Path   string // For non-session events, the path of the watched node.
+				Err    error
+				Server string // For connection events
+			}
+		*/
+
+		for e := range events {
+			println("===============")
+			println("EventNode: " + e.Path)
+			println("State: " + e.State.String())
+
+			if e.Err != nil {
+				log.Println("Err: " + e.Err.Error())
+			}
+
+			switch e.Type {
+
+			// EventNode
+			case zk.EventNodeCreated:
+				log.Println("zk.EventNodeCreated")
+			case zk.EventNodeDeleted:
+				log.Println("zk.EventNodeDeleted")
+			case zk.EventNodeDataChanged:
+				log.Println("zk.EventNodeDataChanged")
+			case zk.EventNodeChildrenChanged:
+				log.Println("zk.EventNodeChildrenChanged")
+
+			// etc event
+			case zk.EventSession:
+				log.Println("zk.EventSession")
+			case zk.EventNotWatching:
+				log.Println("zk.EventNotWatching")
+			}
+
+			children, _, err := conn.Children(e.Path)
+			if err != nil {
+				log.Println(err)
+			}
+
+			for _, child := range children {
+				data, _, err := conn.Get(e.Path + "/" + child)
+				if err != nil {
+					log.Println(err)
+				}
+
+				log.Println("Child: " + child + ", Data: " + string(data))
+			}
 		}
 	}
 }
